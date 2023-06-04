4632b73460e7332d6000458696d5f260bcffa3a9
0gls <0gluis.dev@gmail.com>
2023-06-04 22:00:53 +0200

    Updated Home Page and added AnimalCard component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,20 +4,8 @@
 	import Background from '$lib/components/Background.svelte'
 </script>
 
-<style>
-	.container-scroll {
-		position: relative;
-		scroll-snap-type: y proximity;
-		overflow-y: auto;
-		height: 100vh;
-		width: 100%;
-	}
-</style>
-
-<div class="container-scroll">
-	<div class="container">
-		<Background />
-		<Nav />
-		<slot />
-	</div>
+<div class="container">
+	<Background />
+	<Nav />
+	<slot />
 </div>
