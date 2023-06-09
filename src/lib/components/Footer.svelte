847e9259191e63d0aee43cf3f345edc148e54ae3
0gls <0gluis.dev@gmail.com>
2023-06-10 00:56:19 +0200

    Added component Footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,18 @@
+<style lang="scss">
+  footer {
+    width: 100%;
+    position: absolute;
+    left: 50%;
+    transform: translate(-50%);
+    padding: 16px 32px;
+    background-color: $background;
+  }
+
+  footer > p {
+    color: $gray-600;
+  }
+</style>
+
+<footer>
+  <p class="mb-0">© 2023 WildInfo. All rights reserved.</p>
+</footer>
