6852235252b5d5472d6e6b53b307e52730657da8
0gls <0gluis.dev@gmail.com>
2023-06-10 02:13:15 +0200

    Profile page setup

--- src/routes/profile/+layout.svelte
+++ src/routes/profile/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
+  import Nav from '../../lib/components/Nav.svelte'
   import Background from '$lib/components/Background.svelte'
 </script>
 
+<Nav />
 <Background background="ocean" />
 <slot />
---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,11 @@
+<style lang="scss">
+  section {
+    height: 100vh;
+  }
+</style>
+
+<section>
+  <!-- Aquí va una carta con el nombre de usuario y su foto de perfil -->
+
+  <!-- Aquí una lista igual a la del home pero solo salen las fotos que ha subido el usuario -->
+</section>
